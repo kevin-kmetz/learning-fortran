0f906dbb1173883d6a3227dce03f09c03d95600e
Kevin Kmetz <kev.kmetz@gmail.com>
2022-07-12 22:31:43 -0700

    The game is now minimally playable, though the number literal needs to be modified to be randomly generated.

--- NumberGuessingGame/number_guessing_game.f90
+++ NumberGuessingGame/number_guessing_game.f90
@@ -2,17 +2,31 @@ program number_guessing_game
 
     implicit none
 
+    integer :: guess
+    logical :: is_correct
+
     character(len=:), allocatable :: fmt_i, fmt_o
     fmt_i = "(a, $)"
     fmt_o = "(*(1x, g0))"
+
+    is_correct = .false.
 
     print fmt_o, "Number Guessing Game"
     print fmt_o, "Guess a number between 1 and 100, and you will be told"
     print fmt_o, "whether your guess is too high, too low, or correct."
 
+    do
+
+        guess = solicit_guess()
+        is_correct = evaluate_guess(guess, 72)
+
+        if (is_correct) exit
+
+    end do
+
     contains
 
-    function solicit_guess() return (guess)
+    function solicit_guess() result (guess)
 
         integer :: guess
 
@@ -21,10 +35,12 @@ program number_guessing_game
 
     end function solicit_guess
 
-    function evaluate_guess(guess, winning_number) return (is_correct)
+    function evaluate_guess(guess, winning_number) result (is_correct)
 
         integer, intent(in) :: guess, winning_number
-        logical :: is_correct = .false.
+        logical :: is_correct
+        
+        is_correct = .false.
 
         if (guess < winning_number) then
             print fmt_o, "Too low!"
@@ -34,5 +50,7 @@ program number_guessing_game
             print fmt_o, "Exactly right!"
             is_correct = .true.
         end if
+
+    end function evaluate_guess
 
 end program number_guessing_game
